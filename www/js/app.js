dec88e8d39354b40c153084ad5f1b9042cc54672
chanjunc <chanjunc@gmail.com>
2013-09-25 20:18:49 -0400

    deleted some things i dont need and upgraded to angular 1.2

--- www/js/app.js
+++ www/js/app.js
@@ -7,10 +7,7 @@ function jsonp_callback(data) {
 
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ajoslin.mobile-navigate','ngMobile'])
-    .config(function ($compileProvider){
-        $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-    })
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ajoslin.mobile-navigate','ngTouch', 'ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
         $routeProvider.when('/view1', {templateUrl: 'partials/notificationView.html'});
